ff2eadb3eefe0239af8be0d0311a8f292f35e803
Daniela Priani <dnannis@gmail.com>
2016-07-29 17:01:01 -0300

    Agregamos fecha_faena al tropaBean

--- src/main/java/bean/tropa/TropaBean.java
+++ src/main/java/bean/tropa/TropaBean.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class TropaBean {
 
@@ -13,9 +14,9 @@ public class TropaBean {
 	private int numeroTropa;
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS", locale="es-AR", timezone="America/Argentina/Buenos_Aires")
 	private Date fechaIngreso;
-//	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS", locale="es-AR", timezone="America/Argentina/Buenos_Aires")
-//	private Date fechaFaena;
-//  TODO: se toma del sistema asi que borrar eestos comentarios
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS", locale="es-AR", timezone="America/Argentina/Buenos_Aires")
+	private Date fechaFaena;
 	
 	private int procendeciaId;
 	private int animalesRecibidos;
@@ -42,6 +43,12 @@ public class TropaBean {
 	}
 	public void setFechaIngreso(Date fechaIngreso) {
 		this.fechaIngreso = fechaIngreso;
+	}
+	public Date getFechaFaena() {
+		return fechaFaena;
+	}
+	public void setFechaFaena(Date fechaFaena) {
+		this.fechaFaena = fechaFaena;
 	}
 	public int getProcendeciaId() {
 		return procendeciaId;
@@ -89,6 +96,6 @@ public class TropaBean {
 	@Override
 	public String toString(){
 		Gson gson = new Gson();
-		return gson.toJson(this);
+		return gson.toJson(this, TropaBean.class);
 	}
 }
